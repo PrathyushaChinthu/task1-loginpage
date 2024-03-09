08ff801ca373e392aa31c563e7382d126b848e5b
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-09 21:36:12 +0530

    errors on post dislay data grid mui component not accesing

--- app/users/components/LoginPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoginPage = async () => {
-  return <div>LoginPage</div>;
-};
-
-export default LoginPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,33 +1,48 @@
+'use client'
 import { Table, TableHead } from "@mui/material";
-import React from "react";
-interface Post {
-  id: number;
-  userId: number;
-  title: string;
-}
-const RedirectingPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts: Post[] = await res.json();
-  return (
-    <>
-      <Table>
-        <TableHead>
-          <tr>
-            <th>User Id</th>
-            <th>Title</th>
-          </tr>
-        </TableHead>
-        <tbody>
-          {posts.map((post) => (
-            <tr key={post.id}>
-              <td>{post.userId}</td>
-              <td>{post.title}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </>
-  );
+import React, { useEffect, useState } from "react";
+const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'title', headerName: 'Title', width: 300 },
+];
+const postTableStyles={
+  height: '650px',
+};
+// interface Post {
+//   id: number;
+//   userId: number;
+//   title: string;
+// }
+const RedirectingPage =  () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => setPosts(json))
+}, []);
+
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const posts: Post[] = await res.json();
+  return ()
+  
+//     <DataGrid
+//   rows={rows}
+//   columns={columns}
+//   initialState={{
+//     pagination: {
+//       paginationModel: { page: 0, pageSize: 5 },
+//     },
+//   }}
+//   pageSizeOptions={[5, 10]}
+
+// />
+
+    // <DataTable
+    //         rows={posts}
+    //         columns={columns}
+    //         loading={!posts.length}
+    //         sx={postTableStyles}
+
 };
 
 export default RedirectingPage;
