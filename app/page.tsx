d2f7ca9113309633b7f7f42050741f375029f9d5
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-09 19:48:43 +0530

    Redirecting page work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,101 @@
-import { Link } from "@mui/material";
-import LoginPage from "./users/components/LoginPage";
-
+"use client";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import RedirectingPage from "./users/RedirectingPage";
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    setEmailError(false);
+    setPasswordError(false);
+
+    if (email == "") {
+      setEmailError(true);
+    }
+    if (password == "") {
+      setPasswordError(true);
+    }
+
+    if (email && password) {
+      console.log(email, password);
+    }
+  };
   return (
-    <main>
-      <LoginPage />
-    </main>
+    <Container
+      maxWidth="xs"
+      sx={{
+        backgroundImage:
+          "url(https://www.moople.in/blog/wp-content/uploads/2018/02/New-Project-99.jpg)",
+      }}
+    >
+      <Box
+        height={300}
+        width={300}
+        my={8}
+        bgcolor="#0795C2"
+        justifyContent="center"
+        alignItems="center"
+        gap={4}
+        p={2}
+        sx={{
+          border: "2px solid grey",
+        }}
+      >
+        <FormControl onSubmit={handleSubmit}>
+          <Typography sx={{ mb: 1 }} variant="h4">
+            Welcome
+          </Typography>
+          <FormLabel required>Email</FormLabel>
+          <TextField
+            sx={{ mb: 1 }}
+            color="primary"
+            type="email"
+            fullWidth
+            value={email}
+            error={emailError}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          ></TextField>
+          <FormLabel required>Password</FormLabel>
+          <TextField
+            color="primary"
+            type="password"
+            value={password}
+            error={passwordError}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            fullWidth
+          ></TextField>
+          {/* <Link href="https://jsonplaceholder.typicode.com/posts"> */}
+          <Button
+            type="button"
+            onClick={() => router.push("/users")}
+            sx={{ mt: 2 }}
+            color="primary"
+            variant="contained"
+          >
+            Login
+          </Button>
+          <RedirectingPage />
+          {/* </Link> */}
+        </FormControl>
+      </Box>
+    </Container>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,33 @@
+import { Table, TableHead } from "@mui/material";
 import React from "react";
-
-const page = () => {
-  return <div>Redirecting</div>;
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+}
+const RedirectingPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: Post[] = await res.json();
+  return (
+    <>
+      <Table>
+        <TableHead>
+          <tr>
+            <th>User Id</th>
+            <th>Title</th>
+          </tr>
+        </TableHead>
+        <tbody>
+          {posts.map((post) => (
+            <tr key={post.id}>
+              <td>{post.userId}</td>
+              <td>{post.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
 };
 
-export default page;
+export default RedirectingPage;
