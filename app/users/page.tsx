be9534047afad6ac805c4cbc226ba252e1a26eff
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-10 11:30:51 +0530

    pagenation is done

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import {
   Paper,
   Table,
@@ -6,31 +7,41 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material";
-import React, { useEffect, useState } from "react";
-// const columns = [
-//   { field: "id", headerName: "ID", width: 100 },
-//   { field: "title", headerName: "Title", width: 300 },
-// ];
-// const postTableStyles = {
-//   height: "650px",
-// };
+
 interface Post {
   id: number;
   userId: number;
   title: string;
 }
 
-const RedirectingPage = async () => {
-  //const [posts, setPosts] = useState([]);
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts: Post[] = await res.json();
-  //   useEffect(() => {
-  //     fetch('https://jsonplaceholder.typicode.com/posts')
-  //         .then((response) => response.json())
-  //         .then((json) => setPosts(json))
-  // }, []);ta
+const RedirectingPage = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10); // Adjusted to display 10 rows per page
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data: Post[] = await res.json();
+      setPosts(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -41,7 +52,10 @@ const RedirectingPage = async () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {posts.map((post) => (
+          {(rowsPerPage > 0
+            ? posts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : posts
+          ).map((post) => (
             <TableRow
               key={post.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -54,26 +68,35 @@ const RedirectingPage = async () => {
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[10]} // Only option is 10 rows per page
+        component="div"
+        count={posts.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
-
-  //     <DataGrid
-  //   rows={rows}
-  //   columns={columns}
-  //   initialState={{
-  //     pagination: {
-  //       paginationModel: { page: 0, pageSize: 5 },
-  //     },
-  //   }}
-  //   pageSizeOptions={[5, 10]}
-
-  // />
-
-  // <DataTable
-  //         rows={posts}
-  //         columns={columns}
-  //         loading={!posts.length}
-  //         sx={postTableStyles}
 };
 
 export default RedirectingPage;
+
+//     <DataGrid
+//   rows={rows}
+//   columns={columns}
+//   initialState={{
+//     pagination: {
+//       paginationModel: { page: 0, pageSize: 5 },
+//     },
+//   }}
+//   pageSizeOptions={[5, 10]}
+
+// />
+
+// <DataTable
+//         rows={posts}
+//         columns={columns}
+//         loading={!posts.length}
+//         sx={postTableStyles}
