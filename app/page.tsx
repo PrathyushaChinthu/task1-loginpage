8d533d921bcba7afa9a49aeca5e6228c7e7073cf
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-11 10:43:21 +0530

    modified login page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import {
   Container,
   FormControl,
   FormLabel,
+  Grid,
   TextField,
   Typography,
 } from "@mui/material";
@@ -24,10 +25,10 @@ export default function Home() {
     setEmailError(false);
     setPasswordError(false);
 
-    if (email == "") {
+    if (email === "") {
       setEmailError(true);
     }
-    if (password == "") {
+    if (password === "") {
       setPasswordError(true);
     }
 
@@ -35,70 +36,73 @@ export default function Home() {
       console.log(email, password);
     }
   };
+
   return (
-    <Container
-      maxWidth="xs"
-      sx={{
-        backgroundImage:
-          "url(https://www.moople.in/blog/wp-content/uploads/2018/02/New-Project-99.jpg)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box
-        height={400}
-        width={300}
-        my={8}
-        bgcolor="#0795C2"
-        justifyContent="center"
-        alignItems="center"
-        gap={4}
-        p={2}
-        sx={{
-          border: "2px solid grey",
-        }}
-      >
-        <FormControl onSubmit={handleSubmit}>
-          <Typography sx={{ mb: 1 }} variant="h4">
-            Welcome
-          </Typography>
-          <FormLabel required>Email</FormLabel>
-          <TextField
-            sx={{ mb: 1 }}
-            color="primary"
-            type="email"
-            fullWidth
-            value={email}
-            error={emailError}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          ></TextField>
-          <FormLabel required>Password</FormLabel>
-          <TextField
-            color="primary"
-            type="password"
-            value={password}
-            error={passwordError}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            fullWidth
-          ></TextField>
-          {/* <Link href="https://jsonplaceholder.typicode.com/posts"> */}
-          <Button
-            type="button"
-            onClick={() => router.push("/users")}
-            sx={{ mt: 2 }}
-            color="primary"
-            variant="contained"
-          >
-            Login
-          </Button>
-        </FormControl>
-      </Box>
-    </Container>
+    <Grid container sx={{ height: "100vh" }}>
+      {/* Left Half - Image */}
+      <Grid item xs={12} sm={6}>
+        <Box
+          sx={{
+            backgroundImage:
+              "url(https://www.moople.in/blog/wp-content/uploads/2018/02/New-Project-99.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100%",
+          }}
+        />
+      </Grid>
+
+      {/* Right Half - Login Form */}
+      <Grid item xs={12} sm={6}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+          p={2}
+          bgcolor="#0795C2"
+          sx={{
+            border: "2px solid grey",
+          }}
+        >
+          <FormControl onSubmit={handleSubmit}>
+            <Typography sx={{ mb: 1 }} variant="h4">
+              Welcome
+            </Typography>
+            <FormLabel required>Email</FormLabel>
+            <TextField
+              sx={{ mb: 1 }}
+              color="primary"
+              type="email"
+              fullWidth
+              value={email}
+              error={emailError}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <FormLabel required>Password</FormLabel>
+            <TextField
+              color="primary"
+              type="password"
+              value={password}
+              error={passwordError}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              fullWidth
+            />
+            <Button
+              type="button"
+              onClick={() => router.push("/users")}
+              sx={{ mt: 2, backgroundColor: "#1976D2" }}
+              color="primary"
+              variant="contained"
+            >
+              Login
+            </Button>
+          </FormControl>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
