35726f9d804b69dca5e8fb2d366c1e6d54b9c37b
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-13 19:39:27 +0530

    pagenation and post rendering along with url change

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,23 +1,85 @@
 "use client";
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
+import Link from "next/link";
+import { Post } from "../page";
 
-type Props = {};
+const styles: any = {
+  post: {
+    background: "#378CE7",
+    color: "black",
+    width: "100%", // Adjusted width
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+    padding: "2rem", // Adjusted padding
+    marginBottom: "2rem", // Adjusted margin bottom
+  },
+  button: {
+    padding: "1rem",
+    margin: "1rem",
+    borderRadius: "15px",
+    boxShadow: "#f95959",
+    color: "black",
+    backgroundColor: "#f95959",
+  },
+  link: {
+    color: "black",
+    textDecoration: "none",
+    cursor: "pointer",
+  },
+};
 
-const PostId = (props: Props) => {
+const PostId = () => {
+  const [post, setPost] = useState<Post | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
-  // console.log(router);
-  const params = useParams();
-  console.log(params);
+  const { id } = useParams();
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        const data = await res.json();
+        setPost(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
 
-  return <div>PostId</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <Link href={"/posts"}>
+            <button style={styles.button}>&lt;</button>
+          </Link>
+          <div style={styles.post}>
+            <div>Id: {post?.id}</div>
+            <div>Title: {post?.title}</div>
+            <div>Body: {post?.body}</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PostId;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,95 +1,222 @@
 "use client";
-import React, { useCallback, useEffect, useState, Suspense } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import {
+  Link,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
 
-type Post = {
+export interface Post {
   id: number;
   userId: number;
   title: string;
   body: string;
-};
-
-const styles = {
-  post: {
-    background: "red",
-    margin: 20,
-  },
-};
+}
 
 const Posts = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5); // Adjusted to display 10 rows per page
   const [posts, setPosts] = useState<Post[]>([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const itemsPerPage = 5;
-
-  const fetchPosts = useCallback(async (currentPage: number) => {
-    setIsLoading(true);
-    try {
-      const start = itemsPerPage * (currentPage - 1);
-      const postsReq = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${itemsPerPage}`
-      );
-      const res: Post[] = await postsReq.json();
-      setPosts(res);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  }, []);
-
-  const handleNav = (pageNumber: number) => {
-    const currentPage = page + pageNumber;
-    const params = new URLSearchParams(searchParams.toString());
-    params.set("page", currentPage.toString());
-    router.push(`/posts?${params.toString()}`);
-  };
 
   useEffect(() => {
-    const currentPage = searchParams.get("page") ?? page;
-    setPage(Number(currentPage));
-    fetchPosts(Number(currentPage));
-  }, [searchParams, page, fetchPosts]);
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data: Post[] = await res.json();
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 5));
+    setPage(1);
+  };
 
   return (
-    <Suspense>
-      <div>
-        {isLoading && <div>Loading...</div>}
-        {!isLoading && posts.length > 0 && (
-          <div>
-            {posts.map((post: Post) => (
-              <div style={styles.post} key={post.id}>
-                Id: {post.id} <br />
-                Title: {post.title} <br />
-                Body: {post.body}
-              </div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TableContainer component={Paper} style={{ background: "#378CE7" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="justify">Id</TableCell>
+              <TableCell align="justify">Title</TableCell>
+              <TableCell align="justify">Body</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? posts.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : posts
+            ).map((post) => (
+              <TableRow
+                key={post.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>
+                  <Link
+                    color="inherit"
+                    underline="none"
+                    href={`./posts/${post.id}`}
+                  >
+                    {post.id}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Link
+                    color="inherit"
+                    underline="none"
+                    href={`./posts/${post.id}`}
+                  >
+                    {post.title}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Link
+                    color="inherit"
+                    underline="none"
+                    href={`./posts/${post.id}`}
+                  >
+                    {post.body}
+                  </Link>
+                </TableCell>
+              </TableRow>
             ))}
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                marginBottom: 24,
-              }}
-            >
-              <button disabled={page === 1} onClick={() => handleNav(-1)}>
-                Prev
-              </button>
-              <span>
-                Page: {page} ({(page - 1) * itemsPerPage + 1} to{" "}
-                {page * itemsPerPage})
-              </span>
-              <button onClick={() => handleNav(1)}>Next</button>
-            </div>
-          </div>
-        )}
-        {!isLoading && posts.length === 0 && <div>No Posts</div>}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div>
+        <TablePagination
+          rowsPerPageOptions={[5]} // Only option is 10 rows per page
+          component="div"
+          count={posts.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </div>
-    </Suspense>
+    </div>
   );
 };
-
 export default Posts;
+// import React, { useCallback, useEffect, useState, Suspense } from "react";
+// import { useSearchParams, useRouter } from "next/navigation";
+
+// type Post = {
+//   id: number;
+//   userId: number;
+//   title: string;
+//   body: string;
+// };
+
+// const styles = {
+//   post: {
+//     background: "red",
+//     margin: 20,
+//   },
+// };
+
+// const Posts = () => {
+//   const [posts, setPosts] = useState<Post[]>([]);
+//   const [page, setPage] = useState(1);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+
+//   const itemsPerPage = 5;
+
+//   const fetchPosts = useCallback(async (currentPage: number) => {
+//     setIsLoading(true);
+//     try {
+//       const start = itemsPerPage * (currentPage - 1);
+//       const postsReq = await fetch(
+//         `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${itemsPerPage}`
+//       );
+//       const res: Post[] = await postsReq.json();
+//       setPosts(res);
+//       setIsLoading(false);
+//     } catch (error) {
+//       console.log(error);
+//       setIsLoading(false);
+//     }
+//   }, []);
+
+//   const handleNav = (pageNumber: number) => {
+//     const currentPage = page + pageNumber;
+//     const params = new URLSearchParams(searchParams.toString());
+//     params.set("page", currentPage.toString());
+//     router.push(`/posts?${params.toString()}`);
+//   };
+
+//   useEffect(() => {
+//     const currentPage = searchParams.get("page") ?? page;
+//     setPage(Number(currentPage));
+//     fetchPosts(Number(currentPage));
+//   }, [searchParams, page, fetchPosts]);
+
+//   return (
+//     <Suspense>
+//       <div>
+//         {isLoading && <div>Loading...</div>}
+//         {!isLoading && posts.length > 0 && (
+//           <div>
+//             {posts.map((post: Post) => (
+//               <div style={styles.post} key={post.id}>
+//                 Id: {post.id} <br />
+//                 Title: {post.title} <br />
+//                 Body: {post.body}
+//               </div>
+//             ))}
+//             <div
+//               style={{
+//                 display: "flex",
+//                 justifyContent: "space-around",
+//                 marginBottom: 24,
+//               }}
+//             >
+//               <button disabled={page === 1} onClick={() => handleNav(-1)}>
+//                 Prev
+//               </button>
+//               <span>
+//                 Page: {page} ({(page - 1) * itemsPerPage + 1} to{" "}
+//                 {page * itemsPerPage})
+//               </span>
+//               <button onClick={() => handleNav(1)}>Next</button>
+//             </div>
+//           </div>
+//         )}
+//         {!isLoading && posts.length === 0 && <div>No Posts</div>}
+//       </div>
+//     </Suspense>
+//   );
+// };
+
+// export default Posts;
