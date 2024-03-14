b87db123d3a928319dcc9e421def98c4010cea34
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-15 01:33:39 +0530

    popup page is adjusted

--- app/hook-forms/page.tsx
+++ app/hook-forms/page.tsx
@@ -56,193 +56,193 @@ const Home = (props: Props) => {
     setTabValue(newValue);
   };
   return (
-    <Grid container sx={{ height: "100vh" }}>
-      {/* Left Half - Image */}
-      <Grid item xs={12} sm={8}>
-        <Box
-          sx={{
-            backgroundImage:
-              "url(https://as2.ftcdn.net/v2/jpg/01/19/11/55/1000_F_119115529_mEnw3lGpLdlDkfLgRcVSbFRuVl6sMDty.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "left",
-            height: "100%",
-          }}
-        />
-      </Grid>
-
-      {/* Right Half - Login Form */}
-      <Grid item xs={12} sm={4}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="right"
-          height="100%"
-          width="100%"
-          p={2}
-          sx={{
-            border: "2px solid grey",
-          }}
-          component="form"
-          //onSubmit={handleSubmit(onSubmit)}
+    <Box sx={{ height: "90vh", display: "flex", flexDirection: "column" }}>
+      {/* Button on Top */}
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="10%"
+        width="100%"
+      >
+        <Button
+          sx={{ width: "100%" }}
+          variant="contained"
+          onClick={toggleDialog}
         >
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="space-around"
-            sx={{ mb: 2 }}
+          EXPLORE
+        </Button>
+      </Box>
+      <Box sx={{ flex: 1 }}>
+        <Grid container sx={{ height: "100vh" }}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                backgroundImage:
+                  "url(https://as2.ftcdn.net/v2/jpg/01/19/11/55/1000_F_119115529_mEnw3lGpLdlDkfLgRcVSbFRuVl6sMDty.jpg)",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-around"
+        sx={{ mb: 2 }}
+      >
+        <Dialog open={open} fullWidth onClose={toggleDialog}>
+          <DialogTitle
+            sx={{ color: "green", fontSize: "24px", fontWeight: "bold" }}
           >
-            <Button variant="contained" onClick={toggleDialog}>
-              Welcome
-            </Button>
-            <Dialog open={open} fullWidth onClose={toggleDialog}>
-              <DialogTitle sx={{ color: "green", fontWeight: "bold" }}>
-                Hello
-              </DialogTitle>
-              <DialogContent>
-                <Box>
-                  <Tabs value={tabValue} onChange={handleTabChange}>
-                    <Tab label="Signup" />
-                    <Tab label="Login" />
-                  </Tabs>
-                  {tabValue === 0 && (
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      alignItems="right"
-                      height="100%"
-                      width="90%"
-                      p={2}
-                      sx={{
-                        border: "2px solid grey",
-                      }}
-                      component="form"
-                      onSubmit={handleSubmit(onSubmit)}
-                    >
-                      <Typography sx={{ mb: 1 }} variant="h3">
-                        Welcome
-                      </Typography>
+            UPLABS
+          </DialogTitle>
+          <DialogContent>
+            <Box>
+              <Tabs value={tabValue} onChange={handleTabChange}>
+                <Tab label="Signup" />
+                <Tab label="Login" />
+              </Tabs>
+              {tabValue === 0 && (
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="right"
+                  height="100%"
+                  width="90%"
+                  p={2}
+                  sx={{
+                    border: "2px solid grey",
+                  }}
+                  component="form"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <Typography sx={{ mb: 1 }} variant="h5">
+                    Welcome
+                  </Typography>
 
-                      <TextField
-                        sx={{ mb: 1 }}
-                        {...register("firstName")}
-                        color="primary"
-                        type="string"
-                        name="firstName"
-                        fullWidth
-                        placeholder="John"
-                        label="First name"
-                        helperText={errors?.firstName?.message as any}
-                        error={Boolean(errors?.firstName?.message)}
-                      />
-                      <TextField
-                        sx={{ mb: 1 }}
-                        {...register("lastName")}
-                        color="primary"
-                        type="string"
-                        name="lastName"
-                        fullWidth
-                        placeholder="Williams"
-                        label="Last name"
-                        helperText={errors?.lastName?.message as any}
-                        error={Boolean(errors?.lastName?.message)}
-                      />
-                      <TextField
-                        sx={{ mb: 1 }}
-                        {...register("email")}
-                        color="primary"
-                        type="email"
-                        name="email"
-                        fullWidth
-                        placeholder="abc@gmail.com"
-                        label="Email"
-                        helperText={errors?.email?.message as any}
-                        error={Boolean(errors?.email?.message)}
-                      />
-                      <TextField
-                        color="primary"
-                        {...register("password")}
-                        type="password"
-                        name="password"
-                        fullWidth
-                        placeholder="......"
-                        label="Password"
-                        helperText={errors?.password?.message as any}
-                        error={Boolean(errors?.password?.message)}
-                      />
-                      <Button
-                        type="submit"
-                        style={{ backgroundColor: "#378CE7", flex: "1" }}
-                        sx={{ mt: 2 }}
-                        color="primary"
-                        variant="contained"
-                      >
-                        Create account
-                      </Button>
-                      <Box sx={{ color: "red" }}></Box>
-                    </Box>
-                  )}
-                  {tabValue === 1 && (
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      alignItems="right"
-                      height="100%"
-                      width="90%"
-                      p={2}
-                      sx={{
-                        border: "2px solid grey",
-                      }}
-                      component="form"
-                      onSubmit={handleSubmit(onSubmit)}
-                    >
-                      <Typography sx={{ mb: 1 }} variant="h3">
-                        Welcome Back
-                      </Typography>
-                      <TextField
-                        sx={{ mb: 1 }}
-                        {...register("email")}
-                        color="primary"
-                        type="email"
-                        name="email"
-                        fullWidth
-                        placeholder="abc@gmail.com"
-                        label="Email"
-                        helperText={errors?.email?.message as any}
-                        error={Boolean(errors?.email?.message)}
-                      />
-                      <TextField
-                        color="primary"
-                        {...register("password")}
-                        type="password"
-                        name="password"
-                        fullWidth
-                        placeholder="......"
-                        label="Password"
-                        helperText={errors?.password?.message as any}
-                        error={Boolean(errors?.password?.message)}
-                      />
-                      <Button
-                        type="submit"
-                        style={{ backgroundColor: "#378CE7", flex: "1" }}
-                        sx={{ mt: 2 }}
-                        color="primary"
-                        variant="contained"
-                      >
-                        Login
-                      </Button>
-                      <Box sx={{ color: "red" }}></Box>
-                    </Box>
-                  )}
+                  <TextField
+                    sx={{ mb: 1 }}
+                    {...register("firstName")}
+                    color="primary"
+                    type="string"
+                    name="firstName"
+                    fullWidth
+                    placeholder="John"
+                    label="First name"
+                    helperText={errors?.firstName?.message as any}
+                    error={Boolean(errors?.firstName?.message)}
+                  />
+                  <TextField
+                    sx={{ mb: 1 }}
+                    {...register("lastName")}
+                    color="primary"
+                    type="string"
+                    name="lastName"
+                    fullWidth
+                    placeholder="Williams"
+                    label="Last name"
+                    helperText={errors?.lastName?.message as any}
+                    error={Boolean(errors?.lastName?.message)}
+                  />
+                  <TextField
+                    sx={{ mb: 1 }}
+                    {...register("email")}
+                    color="primary"
+                    type="email"
+                    name="email"
+                    fullWidth
+                    placeholder="abc@gmail.com"
+                    label="Email"
+                    helperText={errors?.email?.message as any}
+                    error={Boolean(errors?.email?.message)}
+                  />
+                  <TextField
+                    color="primary"
+                    {...register("password")}
+                    type="password"
+                    name="password"
+                    fullWidth
+                    placeholder="......"
+                    label="Password"
+                    helperText={errors?.password?.message as any}
+                    error={Boolean(errors?.password?.message)}
+                  />
+                  <Button
+                    type="submit"
+                    style={{ backgroundColor: "#378CE7", flex: "1" }}
+                    sx={{ mt: 2 }}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Create account
+                  </Button>
+                  <Box sx={{ color: "red" }}></Box>
                 </Box>
-              </DialogContent>
-            </Dialog>
-          </Box>
-        </Box>
-      </Grid>
-    </Grid>
+              )}
+              {tabValue === 1 && (
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="right"
+                  height="100%"
+                  width="90%"
+                  p={2}
+                  sx={{
+                    border: "2px solid grey",
+                  }}
+                  component="form"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <Typography sx={{ mb: 1 }} variant="h5">
+                    Welcome Back
+                  </Typography>
+                  <TextField
+                    sx={{ mb: 1 }}
+                    {...register("email")}
+                    color="primary"
+                    type="email"
+                    name="email"
+                    fullWidth
+                    placeholder="abc@gmail.com"
+                    label="Email"
+                    helperText={errors?.email?.message as any}
+                    error={Boolean(errors?.email?.message)}
+                  />
+                  <TextField
+                    color="primary"
+                    {...register("password")}
+                    type="password"
+                    name="password"
+                    fullWidth
+                    placeholder="......"
+                    label="Password"
+                    helperText={errors?.password?.message as any}
+                    error={Boolean(errors?.password?.message)}
+                  />
+                  <Button
+                    type="submit"
+                    style={{ backgroundColor: "#378CE7", flex: "1" }}
+                    sx={{ mt: 2 }}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Login
+                  </Button>
+                  <Box sx={{ color: "red" }}></Box>
+                </Box>
+              )}
+            </Box>
+          </DialogContent>
+        </Dialog>
+      </Box>
+    </Box>
   );
 };
 export default Home;
