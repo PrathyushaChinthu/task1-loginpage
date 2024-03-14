9cf6296522f2aaaa0fd90150229e31866b1865ca
Ganesh Munisifreddy <ganeshmunisif@gmail.com>
2024-03-14 22:42:06 +0530

    add: demo route

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,12 +1,41 @@
-import { Box, Button } from '@mui/material';
-import React from 'react';
+'use client';
+import { Box, Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import React, { useState } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 type Props = {};
 
 const DemoPage = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const toggleDialog = () => {
+    setOpen(!open);
+  };
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+
   return (
     <Box sx={{ m: 4 }}>
-      <Button variant='contained'>Login</Button>
+      <Button variant='contained' onClick={toggleDialog}>
+        Login
+      </Button>
+      <Dialog open={open} fullWidth onClose={toggleDialog}>
+        <DialogTitle>Welcome</DialogTitle>
+        <DialogContent>
+          <Box>
+            <Tabs value={tabValue} onChange={handleTabChange}>
+              <Tab label='Login' />
+              <Tab label='Signup' />
+            </Tabs>
+            {tabValue === 0 && <div>Login Form</div>}
+            {tabValue === 1 && <div>Signup Form</div>}
+          </Box>
+        </DialogContent>
+      </Dialog>
     </Box>
   );
 };
