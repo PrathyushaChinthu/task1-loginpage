1a3315c12b8284917c1134712dde9b77d5fc95e9
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-03-14 23:03:54 +0530

    signup page working

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             />
             <Button
               type="button"
-              onClick={() => router.push("/hookForms")}
+              onClick={() => router.push("/hook-forms")}
               style={{ backgroundColor: "#378CE7", flex: "1" }}
               sx={{ mt: 2 }}
               color="primary"
